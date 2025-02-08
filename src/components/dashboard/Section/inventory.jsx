4c35e7c61ada2c34b920e5bc1d5824c884d5dde3
Shu1237 <ttei8191@gmail.com>
2025-02-08 08:03:51 +0700

    update

--- src/components/dashboard/Section/inventory.jsx
+++ src/components/dashboard/Section/inventory.jsx
@@ -1,5 +1,5 @@
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
-
+import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
+import KeyboardArrowLeftOutlinedIcon from '@mui/icons-material/KeyboardArrowLeftOutlined';
 
 const inventory = () => {
     const products = [
@@ -128,7 +128,7 @@ const inventory = () => {
               className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Previous</span>
-              <ChevronLeftIcon aria-hidden="true" className="size-5" />
+              <KeyboardArrowLeftOutlinedIcon aria-hidden="true" className="size-5" />
             </a>
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
             <a
@@ -176,7 +176,7 @@ const inventory = () => {
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Next</span>
-              <ChevronRightIcon aria-hidden="true" className="size-5" />
+              <KeyboardArrowRightOutlinedIcon aria-hidden="true" className="size-5" />
             </a>
           </nav>
         </div>

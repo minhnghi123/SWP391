e1c6f1eb4669990571c837aef5ae994e255fd0bd
Alien <minhhnghiii@gmail.com>
2025-06-19 18:41:07 +0700

    feat(store): update reducer

--- src/components/redux/store.jsx
+++ src/components/redux/store.jsx
@@ -3,27 +3,19 @@ import childrenSelectSlice from "./reducers/selectChildren";
 import selectVaccineSlice from "./reducers/SelectVaccine";
 import accountSlice from "./reducers/accountSlice";
 import menthodPaymentSlice from "./reducers/methodPaymentlice";
-import dataUserSlice from "./reducers/dataUser";
 import feedbackTrackingSlice from "./reducers/feedbackTracking";
 import historyTrackingSlice from "./reducers/historyTrackingSlice";
 import locationSlice from "./reducers/locationBooking";
 const store = configureStore({
-    reducer: {
-        account: accountSlice.reducer,
-        vaccine: selectVaccineSlice.reducer,
-        children: childrenSelectSlice.reducer,
-        methodPayment: menthodPaymentSlice.reducer,
-        profilechildren: childrenSelectSlice.reducer,
-        location: locationSlice.reducer,
-
-
-
-
-        // api
-        // dataUser:dataUserSlice.reducer
-        feedbackTracking: feedbackTrackingSlice.reducer,
-        historyTracking: historyTrackingSlice.reducer
-
-    },
-})
-export default store
+  reducer: {
+    account: accountSlice.reducer,
+    vaccine: selectVaccineSlice.reducer,
+    children: childrenSelectSlice.reducer,
+    methodPayment: menthodPaymentSlice.reducer,
+    profilechildren: childrenSelectSlice.reducer,
+    location: locationSlice.reducer,
+    feedbackTracking: feedbackTrackingSlice.reducer,
+    historyTracking: historyTrackingSlice.reducer,
+  },
+});
+export default store;

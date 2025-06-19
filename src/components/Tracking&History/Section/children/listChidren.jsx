080f11f431745d7397a93d8149f0bff19dc44786
Alien <minhhnghiii@gmail.com>
2025-06-19 19:09:33 +0700

    fix( ci/cd ) : fix build

--- src/components/Tracking&History/Section/children/listChidren.jsx
+++ src/components/Tracking&History/Section/children/listChidren.jsx
@@ -1,194 +1,213 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 
-import HeaderChildren from './HeaderChildren';
-import ChildCart from './ChildCart';
-import ModalCrudChild from './ModalCrudChild';
-import ModalDelete from './ModalDelete';
-import formatDate from '../../../../utils/Date';
-import useAxios from '../../../../utils/useAxios'
-const url = import.meta.env.VITE_BASE_URL_DB
+import HeaderChildren from "./HeaderChildren";
+import ChildCart from "./ChildCart";
+import ModalCrudChild from "./modalCrudChild";
+import ModalDelete from "./ModalDelete";
+import formatDate from "../../../../utils/Date";
+import useAxios from "../../../../utils/useAxios";
+const url = import.meta.env.VITE_BASE_URL_DB;
 export default function ListChildren({ id }) {
-    const api = useAxios()
-    const [listChildren, setListChildren] = useState([]);
-    const [filteredChildren, setFilteredChildren] = useState([]);
-    const [currentChild, setCurrentChild] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isEditing, setIsEditing] = useState(false);
-    const [activeFilter, setActiveFilter] = useState('all');
-    const [deleteId, setDeleteId] = useState(null);
-    const [showModalDelete, setShowModalDelete] = useState(false);
-    const [trigger, setTrigger] = useState(false);
-    const [err, setErr] = useState("");
-    const defaultChild = { parentId: id, name: "", dateOfBirth: "", gender: "" };
-    const [isLoading, setIsLoading] = useState(false);
-   
-    //fetch data
-    useEffect(() => {
-        if (!id) return;
-        const fetchChildren = async () => {
-            try {
-                const res = await api.get(`${url}/Child/get-child-by-parents-id/${id}`);
-                if (res.status === 200) setListChildren(res.data);
-            } catch (error) {
-                // setErr("Failed to fetch children data.");
-                console.log(error);
-            }
-        };
-        fetchChildren();
-    }, [id, trigger]);
+  const api = useAxios();
+  const [listChildren, setListChildren] = useState([]);
+  const [filteredChildren, setFilteredChildren] = useState([]);
+  const [currentChild, setCurrentChild] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [activeFilter, setActiveFilter] = useState("all");
+  const [deleteId, setDeleteId] = useState(null);
+  const [showModalDelete, setShowModalDelete] = useState(false);
+  const [trigger, setTrigger] = useState(false);
+  const [err, setErr] = useState("");
+  const defaultChild = { parentId: id, name: "", dateOfBirth: "", gender: "" };
+  const [isLoading, setIsLoading] = useState(false);
 
-    //filter data
-    useEffect(() => {
-        setFilteredChildren(listChildren);
-    }, [listChildren]);
-
-    //open modal add child
-    const handleOpenAddChildModal = () => {
-        setCurrentChild(defaultChild);
-        setIsEditing(false);
-        setIsModalOpen(true);
+  //fetch data
+  useEffect(() => {
+    if (!id) return;
+    const fetchChildren = async () => {
+      try {
+        const res = await api.get(`${url}/Child/get-child-by-parents-id/${id}`);
+        if (res.status === 200) setListChildren(res.data);
+      } catch (error) {
+        // setErr("Failed to fetch children data.");
+        console.log(error);
+      }
     };
+    fetchChildren();
+  }, [id, trigger]);
 
+  //filter data
+  useEffect(() => {
+    setFilteredChildren(listChildren);
+  }, [listChildren]);
 
-    const handleCreateChild = async (e) => {
-        setIsLoading(true);
-        e.preventDefault();
-        if (![0, 1].includes(Number(currentChild.gender))) return setErr("Please choose a valid gender.");
+  //open modal add child
+  const handleOpenAddChildModal = () => {
+    setCurrentChild(defaultChild);
+    setIsEditing(false);
+    setIsModalOpen(true);
+  };
 
-        try {
-            const res = await api.post(`${url}/Child/create-child`, currentChild);
-            if (res.status === 200) {
-                // setListChildren([...listChildren, {
-                //     ...currentChild,
-                //     status: "Active"
-                // }]);
-                setIsModalOpen(false);
-                toast.success("Child added successfully!");
-                setTrigger(prev => !prev)
-            }
-        } catch (error) {
-            toast.error(error.response?.data?.message || "Failed to create child.");
-        } finally {
-            setIsLoading(false);
-        }
-    };
+  const handleCreateChild = async (e) => {
+    setIsLoading(true);
+    e.preventDefault();
+    if (![0, 1].includes(Number(currentChild.gender)))
+      return setErr("Please choose a valid gender.");
 
+    try {
+      const res = await api.post(`${url}/Child/create-child`, currentChild);
+      if (res.status === 200) {
+        // setListChildren([...listChildren, {
+        //     ...currentChild,
+        //     status: "Active"
+        // }]);
+        setIsModalOpen(false);
+        toast.success("Child added successfully!");
+        setTrigger((prev) => !prev);
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Failed to create child.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    const handleOpenEditChildModal = (child) => {
-        setCurrentChild({ ...child, dateOfBirth: formatDate(child.dateOfBirth) });
-        setIsEditing(true);
-        setIsModalOpen(true);
-    };
+  const handleOpenEditChildModal = (child) => {
+    setCurrentChild({ ...child, dateOfBirth: formatDate(child.dateOfBirth) });
+    setIsEditing(true);
+    setIsModalOpen(true);
+  };
 
+  const handleUpdateChild = async (e) => {
+    setIsLoading(true);
+    e.preventDefault();
+    if (![0, 1].includes(Number(currentChild.gender)))
+      return setErr("Please choose a valid gender.");
+    if (
+      listChildren.find(
+        (child) =>
+          child.id === currentChild.id &&
+          child.status.toLowerCase() === "tracking"
+      )
+    ) {
+      toast.error(
+        "This child is currently being tracked. Please complete the tracking process before updating."
+      );
+      setIsLoading(false);
+      setIsModalOpen(false);
+      return;
+    }
+    try {
+      const res = await api.put(
+        `${url}/Child/update-child/${currentChild.id}`,
+        currentChild
+      );
+      if (res.status === 200) {
+        // setListChildren(listChildren.map(child => child.id === currentChild.id ? currentChild : child));
+        setIsModalOpen(false);
+        toast.success("Child updated successfully!");
+        setTrigger((prev) => !prev);
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Failed to update child.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    const handleUpdateChild = async (e) => {
-        setIsLoading(true);
-        e.preventDefault();
-        if (![0, 1].includes(Number(currentChild.gender))) return setErr("Please choose a valid gender.");
-        if (listChildren.find(child => child.id === currentChild.id && child.status.toLowerCase() === "tracking")) {
-            toast.error("This child is currently being tracked. Please complete the tracking process before updating.");
-            setIsLoading(false);
-            setIsModalOpen(false);
-            return;
-        }
-        try {
-            const res = await api.put(`${url}/Child/update-child/${currentChild.id}`, currentChild);
-            if (res.status === 200) {
-                // setListChildren(listChildren.map(child => child.id === currentChild.id ? currentChild : child));
-                setIsModalOpen(false);
-                toast.success("Child updated successfully!");
-                setTrigger(prev => !prev)
-            }
-        } catch (error) {
-            toast.error(error.response?.data?.message || "Failed to update child.");
-        } finally {
-            setIsLoading(false);
-        }
-    };
+  const handleOpenDeleteChildModal = (id) => {
+    setDeleteId(id);
+    setShowModalDelete(true);
+  };
 
+  const handleDeleteChild = async () => {
+    setIsLoading(true);
+    if (
+      listChildren.find(
+        (child) =>
+          child.id === deleteId && child.status.toLowerCase() === "tracking"
+      )
+    ) {
+      toast.error(
+        "This child is currently being tracked. Please complete the tracking process before deleting."
+      );
+      setShowModalDelete(false);
+      setIsLoading(false);
+      return;
+    }
+    try {
+      const res = await api.patch(`${url}/Child/soft-delete-child/${deleteId}`);
+      if (res.status === 200) {
+        setListChildren(listChildren.filter((child) => child.id !== deleteId));
+        toast.success("Child deleted successfully!");
+        setShowModalDelete(false);
+        setDeleteId(null);
+        // setTrigger(prev => !prev)
+      }
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Failed to delete child.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-    const handleOpenDeleteChildModal = (id) => {
-        setDeleteId(id);
-        setShowModalDelete(true);
-    };
+  const handleFilter = (status) => {
+    setActiveFilter(status);
+    if (status === "all") {
+      setFilteredChildren(listChildren);
+    } else {
+      setFilteredChildren(
+        listChildren.filter(
+          (child) => Number(child.gender) === (status === "male" ? 0 : 1)
+        )
+      );
+    }
+  };
 
+  return (
+    <>
+      <ModalDelete
+        isOpen={showModalDelete}
+        onClose={() => setShowModalDelete(false)}
+        onConfirm={handleDeleteChild}
+        isLoading={isLoading}
+      />
 
-    const handleDeleteChild = async () => {
-        setIsLoading(true);
-        if (listChildren.find(child => child.id === deleteId && child.status.toLowerCase() === "tracking")) {
-            toast.error("This child is currently being tracked. Please complete the tracking process before deleting.");
-            setShowModalDelete(false);
-            setIsLoading(false);
-            return;
-        }
-        try {
-            const res = await api.patch(`${url}/Child/soft-delete-child/${deleteId}`);
-            if (res.status === 200) {
-                setListChildren(listChildren.filter(child => child.id !== deleteId));
-                toast.success("Child deleted successfully!");
-                setShowModalDelete(false);
-                setDeleteId(null);
-                // setTrigger(prev => !prev)
-            }
-        } catch (error) {
-            toast.error(error.response?.data?.message || "Failed to delete child.");
-        } finally {
-            setIsLoading(false);
-        }
+      <HeaderChildren
+        activeFilter={activeFilter}
+        handleAddChild={handleOpenAddChildModal}
+        handleFilter={handleFilter}
+      />
 
-    };
+      <ChildCart
+        handleAddChild={handleOpenAddChildModal}
+        handleEditChild={handleOpenEditChildModal}
+        sortchildren={filteredChildren}
+        handleDeleteChild={handleOpenDeleteChildModal}
+      />
 
-
-    const handleFilter = (status) => {
-        setActiveFilter(status);
-        if (status === 'all') {
-            setFilteredChildren(listChildren);
-        } else {
-            setFilteredChildren(listChildren.filter(child => Number(child.gender) === (status === 'male' ? 0 : 1)));
-        }
-    };
-
-    return (
-        <>
-
-
-
-            <ModalDelete
-                isOpen={showModalDelete}
-                onClose={() => setShowModalDelete(false)}
-                onConfirm={handleDeleteChild}
-                isLoading={isLoading}
-            />
-
-
-            <HeaderChildren
-                activeFilter={activeFilter}
-                handleAddChild={handleOpenAddChildModal}
-                handleFilter={handleFilter}
-            />
-
-
-            <ChildCart
-                handleAddChild={handleOpenAddChildModal}
-                handleEditChild={handleOpenEditChildModal}
-                sortchildren={filteredChildren}
-                handleDeleteChild={handleOpenDeleteChildModal}
-            />
-
-
-            {isModalOpen && (
-                <ModalCrudChild
-                    setIsModalOpen={setIsModalOpen}
-                    currentChild={currentChild}
-                    isEditing={isEditing}
-                    handleSaveChild={isEditing ? handleUpdateChild : handleCreateChild}
-                    handleInputChange={(e) => setCurrentChild(prev => ({ ...prev, [e.target.name]: e.target.name === "gender" ? Number(e.target.value) : e.target.value }))}
-                    err={err}
-                    isLoading={isLoading}
-                />
-            )}
-        </>
-    );
+      {isModalOpen && (
+        <ModalCrudChild
+          setIsModalOpen={setIsModalOpen}
+          currentChild={currentChild}
+          isEditing={isEditing}
+          handleSaveChild={isEditing ? handleUpdateChild : handleCreateChild}
+          handleInputChange={(e) =>
+            setCurrentChild((prev) => ({
+              ...prev,
+              [e.target.name]:
+                e.target.name === "gender"
+                  ? Number(e.target.value)
+                  : e.target.value,
+            }))
+          }
+          err={err}
+          isLoading={isLoading}
+        />
+      )}
+    </>
+  );
 }
